90cd3b75f04ef04fabaefb6626854e4bda414c8d
Karthick J <47981480+karthickjayakumar@users.noreply.github.com>
2024-11-18 17:04:09 +0530

    Add files via upload

---
+++ Prj4-Personalized real estate agent/HomeMatch.py
@@ -0,0 +1,9 @@
+#A starter file for the HomeMatch application if you want to build your solution in a Python program instead of a notebook. 
+
+import os
+
+os.environ["OPENAI_API_KEY"] = "voc-169118258412667735687586702328a53a620.77003172"
+os.environ["OPENAI_API_BASE"] = "https://openai.vocareum.com/v1"
+
+from langchain.llms import OpenAI
+
