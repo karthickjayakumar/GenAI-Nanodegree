b88acdc3974b86f87d230ebf4c95a3328a73fbbc
Karthick J <47981480+karthickjayakumar@users.noreply.github.com>
2024-11-18 17:04:28 +0530

    Update HomeMatch.py

--- Prj4-Personalized real estate agent/HomeMatch.py
+++ Prj4-Personalized real estate agent/HomeMatch.py
@@ -2,7 +2,7 @@
 
 import os
 
-os.environ["OPENAI_API_KEY"] = "voc-169118258412667735687586702328a53a620.77003172"
+os.environ["OPENAI_API_KEY"] = ""
 os.environ["OPENAI_API_BASE"] = "https://openai.vocareum.com/v1"
 
 from langchain.llms import OpenAI
